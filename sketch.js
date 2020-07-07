8a418309bf5977e3c394f60b3ffb067800ba17db
MihikaDevireddy <mdevireddy@cherrycreekschools.org>
2020-07-07 09:45:35 -0600

    created constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var logA;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +39,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    logA = new Log(200,200,200,PI/4);
+
+    chain = new Chain(bird.body,logA.body)
 }
 
 function draw(){
@@ -61,4 +67,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    logA.display();
+
+    chain.display();
 }
